44b12e7d779d8b333675e95a136369a2ac5d15dd
jp-caba <122952053+jp-caba@users.noreply.github.com>
2024-10-11 13:23:52 -0500

    Create APP.js

---
+++ Frontend/APP.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import OnboardingScreen from './screens/index'; // Onboarding screen
+import LoginScreen from './screens/login'; // Login screen
+import SignUpScreen from './screens/signup'; // Sign-up screen
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Onboarding">
+        <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
